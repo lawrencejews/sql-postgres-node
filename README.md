f4833d043dfc68952ee70cbb5230bc6d133b7c69
lawrencejews <lawrencejews1@gmail.com>
2023-02-23 16:21:37 +0900

    sql-postgres

---
+++ README.md
@@ -0,0 +1,12 @@
+### Postgres-sql
+
+#### Docker: Database
+- Create a container running postgres-14:`docker run -e POSTGRES_PASSWORD=lol --name=pg --rm -d -p 5432:5432 postgres:14`.
+- Directly to postgres: `docker exec -u postgres -it pg psql`.
+- Check for commands for postgres to navigate through the database: HELP `\?`.
+- Create your database to work from `CREATE DATABASE name` then switch to it `\c name`.
+- Postgres with node: `https://node-postgres.com`.
+#### Docker: JSONB
+- Create  a new container `docker run -e POSTGRES_PASSWORD=lol --name=sql -p 5432:5432 -d --rm btholt/complete-intro-to-sql`
+- Run postgres: `docker exec -u postgres -it sql psql`.
+
---
+++ ingredients/api.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+
+// client side static assets
+router.get("/", (_, res) => res.sendFile(path.join(__dirname, "./index.html")));
+router.get("/client.js", (_, res) =>
+  res.sendFile(path.join(__dirname, "./client.js"))
+);
+
+// connect to postgres
+const pg = require("pg");
+const pool = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "recipeguru",
+  password: "lol",
+  port: 5432,
+});
+
+router.get("/type", async (req, res) => {
+  const { type } = req.query;
+  console.log("get ingredients", type);
+
+  const { rows } = await pool.query(
+    "SELECT * FROM ingredients WHERE type = $1",
+    [type]
+  );
+  res.json({ rows });
+});
+
+router.get("/search", async (req, res) => {
+  let { term, page } = req.query;
+  page = page ? page : 0;
+  console.log("search ingredients", term, page);
+
+  // return all columns as well as the count of all rows as total_count
+  // make sure to account for pagination and only return 5 rows at a time
+
+  let whereClause = "";
+  const params = [page * 5];
+
+  if (term) {
+    whereClause = `WHERE CONCAT(title, type) ILIKE $2`;
+    params.push(`%${term}%`);;
+  }
+
+  const { rows } = await pool.query(
+    `SELECT * FROM ingredients ${whereClause} OFFSET $1 LIMIT 5`,
+    params
+  );
+
+  res.json({ rows });
+});
+
+module.exports = router;
